aced346548705da197bcac4ed8aa8108eaa4c66f
kangcodeis <miiren543@daum.net>
2022-10-10 20:34:46 +0900

    jwt 추가

--- front/pages/join/index.js
+++ front/pages/join/index.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import axios from "axios";
 import styled from "styled-components";
 
 const Warpper = styled.div`
@@ -8,27 +10,50 @@ const Warpper = styled.div`
 `;
 
 const Join = () => {
+  // const [click, setClick] = useState();
+
+  const [input, setInput] = useState();
+  const inputChange = (e) => {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+    console.log("input 확인용??", input);
+  };
+
+  const btnJoin = async (e) => {
+    e.preventDefault();
+    const result = await axios.post("http://localhost:4001/user/join", input);
+    console.log("result????:", result);
+  };
   return (
     <Warpper>
       <h1>회원가입페이지입니다</h1>
       <ul>
         <li>
-          이름 : <input></input>
+          이름 : <input name="userid" onChange={inputChange}></input>
         </li>
         <li>
-          닉네임 :<input></input>
+          닉네임 :<input name="nickname" onChange={inputChange}></input>
         </li>
         <li>
-          이메일 :<input></input>
+          이메일 :<input name="email" onChange={inputChange}></input>
         </li>
         <li>
-          전화번호 :<input></input>
+          전화번호 :<input name="tell" onChange={inputChange}></input>
         </li>
         <li>
-          주소 :<input></input>
+          주소 :<input name="address" onChange={inputChange}></input>
         </li>
         <li>
-          <button>가입</button>
+          이름 :<input name="username" onChange={inputChange}></input>
+        </li>
+        <li>
+          성별 :<input name="gender" onChange={inputChange}></input>
+        </li>
+        <li>
+          비밀번호 :<input name="userpw" onChange={inputChange}></input>
+        </li>
+        <li>
+          <input type="submit" value="가입" onClick={btnJoin}></input>
         </li>
       </ul>
     </Warpper>
