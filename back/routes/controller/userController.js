01a9275e074238ded1b39446c60d2b6c663d5543
kangcodeis <miiren543@daum.net>
2022-10-10 11:00:35 +0900

    user 추가

--- back/routes/controller/userController.js
+++ back/routes/controller/userController.js
@@ -1,2 +1,29 @@
+const User = require("../../models/user");
+
+exports.createUser = async (req, res) => {
+  try {
+    const query = "INSERT";
+    console.log("로그 확인용");
+    const { userid, userpw, username, nickname, gender, tell, email, address } =
+      req.body;
+    // Create a new user
+    const jane = await User.create({
+      userid,
+      userpw,
+      username,
+      nickname,
+      gender,
+      tell,
+      email,
+      address,
+    });
+    console.log("Jane's auto-generated ID:", jane.id);
+    res.json(jane);
+  } catch (e) {
+    console.log("에러발생", e.message);
+    res.json(e);
+  }
+};
+
 exports.login = (req, res) => {};
 exports.join = (req, res) => {};
--- back/routes/index.js
+++ back/routes/index.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const userRouter = require("./router/userRouter");
 const boardRouter = require("./router/boardRouter");
 
+router.use("/user", userRouter);
+// router.use("/board", boardRouter);
+
 router.use("/", (req, res) => {
   console.log("홈입니다");
 });
-
-router.use("/user", userRouter);
-// router.use("/board", boardRouter);
 
 module.exports = router;
