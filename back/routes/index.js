0be5ddb23aaae9218c5b672263cc18043f776722
lhj12ps <lhj12ps@gmail.com>
2022-11-01 00:01:04 +0900

    AWS 배포위해 repository 분리

--- back/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userRouter = require("./router/userRouter");
-const boardRouter = require("./router/boardRouter");
-
-router.use("/user", userRouter);
-router.use("/board", boardRouter);
-
-module.exports = router;
--- back/routes/router/userRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const loginController = require("../controller/userController");
-const userController = require("../controller/userController");
-
-router.post("/login", loginController.login);
-router.post("/join", userController.join);
-
-module.exports = router;
